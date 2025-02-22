22729e978d097c8a6cffdaed749cc90b7c7b8027
Naufal Alief <naufalalief087@gmail.com>
2025-02-23 00:09:26 +0700

    test

--- components/index.js
+++
@@ -1,6 +0,0 @@
-import './AddExpenseForm.js';
-import './ExpenseTable.js';
-import './ExpenseSummary.js';
-import './FilterForm.js';
-import './NavBar.js';
-import './Footer.js';
--- script/index.js
+++
@@ -1,8 +0,0 @@
-import '../style/output.css';
-import '../components/index.js';
-import * as script from './script.js';
-
-document.addEventListener('DOMContentLoaded', () => {
-  script.default(); // Memanggil fungsi app
-  Object.assign(window, script); // Menambahkan semua ekspor dari script.js ke konteks global
-});
